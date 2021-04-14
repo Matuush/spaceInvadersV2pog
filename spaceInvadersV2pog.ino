ea1ea9a5c2a79c64f1fffe53964d3655610da8f7
Matuush <56359492+Matuush@users.noreply.github.com>
2021-04-14 15:25:46 +0200

    uhhhh workang uhhhh the fucken uhhhhh

--- spaceInvadersV2pog.ino
+++ spaceInvadersV2pog.ino
@@ -11,9 +11,8 @@ MaxMatrix disp = MaxMatrix(DIN, CS, CLK, 1);
 unsigned int bulletCount = 0;
 unsigned int bulletX[8] = {0, 0, 0, 0, 0, 0, 0, 0}, bulletY[8] = {6, 6, 6, 6, 6, 6, 6, 6};
 #define newBullet(x) {bulletX[bulletCount] = x; bulletCount++;}
-#define setBullet(i, ii) {bulletX[(i)] = bulletX[(ii)], bulletY[(i)] = bulletY[(ii)];}
 #define resetBullet(i) {bulletCount--; \
-  for(unsigned int ii = i; ii < bulletCount - 1; ii++) {setBullet(ii, ii+1);} \
+  for(unsigned int ii = i; ii < bulletCount; ii++) {bulletX[(ii)] = bulletX[(ii+1)], bulletY[(ii)] = bulletY[(ii+1)];} \
   bulletY[bulletCount] = 6;}
 #define movBullet(i) {if(bulletY[i] == 0) {resetBullet(i)} else bulletY[i]--;}
 #define movBullets() {for(unsigned int i = 0; i < bulletCount; i++) {movBullet(i)}}
@@ -26,12 +25,11 @@ const unsigned int shipY = 7;
 
 // Enemy
 unsigned int enemyCount = 0;
-unsigned int enemyX[8] = {0, 0, 0, 0, 0, 0, 0, 0}, enemyY[8] = {6, 6, 6, 6, 6, 6, 6, 6};
+unsigned int enemyX[8] = {0, 0, 0, 0, 0, 0, 0, 0}, enemyY[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 #define newEnemy(x) {enemyX[enemyCount] = x; enemyCount++;}
-#define setEnemy(i, ii) {enemyX[(i)] = enemyX[(ii)], enemyY[(i)] = enemyY[(ii)];}
 #define resetEnemy(i) {enemyCount--; \
-  for(unsigned int ii = i; ii < enemyCount - 1; ii++) {setEnemy(ii, ii+1);} \
-  enemyY[enemyCount] = 6;}
+  for(unsigned int ii = i; ii < enemyCount; ii++) {enemyX[(ii)] = enemyX[(ii+1)], enemyY[(ii)] = enemyY[(ii+1)];} \
+  enemyY[enemyCount] = 0;}
 #define movEnemy(i) {if(enemyY[i] == 7) {resetEnemy(i)} else enemyY[i]++;}
 #define movEnemies() {for(unsigned int i = 0; i < enemyCount; i++) {movEnemy(i)}}
 
@@ -40,13 +38,15 @@ unsigned int enemyX[8] = {0, 0, 0, 0, 0, 0, 0, 0}, enemyY[8] = {6, 6, 6, 6, 6, 6
 #define prtBullet(i) {disp.setDot(bulletX[i], bulletY[i], true);}
 #define prtBullets() {for(unsigned int i = 0; i < bulletCount; i++) prtBullet(i);}
 #define prtEnemy(i) {disp.setDot(enemyX[i], enemyY[i], true);}
-#define prtEnemy() {for(unsigned int i = 0; i < enemyCount; i++) prtEnemy(i);}
+#define prtEnemies() {for(unsigned int i = 0; i < enemyCount; i++) prtEnemy(i);}
 #define prtArr(vjecY, vjecX) {Serial.print(vjecY[0]); Serial.print(vjecY[1]); Serial.print(vjecY[2]); Serial.print(vjecY[3]); Serial.print(vjecY[4]); Serial.print(vjecY[5]); Serial.print(vjecY[6]); Serial.println(vjecY[7]);\
   Serial.print(vjecX[0]); Serial.print(vjecX[1]); Serial.print(vjecX[2]); Serial.print(vjecX[3]); Serial.print(vjecX[4]); Serial.print(vjecX[5]); Serial.print(vjecX[6]); Serial.println(vjecX[7]);}
 
 // uhhh general
+unsigned long time = 0;
+#define DELAY 3000
 unsigned int score = 0, highScore = 0;
-#define reset() score = 0, shipX = 3, shipLife = 3;
+#define reset() score = 0, shipX = 3, shipLife = 3, bulletY[0] = 6, bulletY[1] = 6, bulletY[2] = 6, bulletY[3] = 6, bulletY[4] = 6, bulletY[5] = 6, bulletY[6] = 6, bulletY[7] = 6, enemyY[0] = 0, enemyY[1] = 0, enemyY[2] = 0, enemyY[3] = 0, enemyY[4] = 0, enemyY[5] = 0, enemyY[6] = 0, enemyY[7] = 0;
 
 // UHHHHHHHHH
 void setup() {
@@ -57,18 +57,20 @@ void setup() {
   Serial.begin(9600);
 }
 
-unsigned long time = 0;
 void loop() {
   time++;
+  if(time % DELAY == 0){
     mov();
     movBullets();
-    if(time % 4 == 0 && analogRead(JOY_Y) < 200) shoot();
-
-  disp.clear();
-  prtShip();
-  prtBullets();
+    movEnemies();
+    //if(time % (4 * DELAY) == 0 && analogRead(JOY_Y) < 200) shoot();
+    if(time % (5 * DELAY) == 0) newEnemy(random(0, 8));
+    disp.clear();
+    prtShip();
+    prtBullets();
+    prtEnemies();
+  }
 
   if(score > highScore) highScore = score;
   if(shipLife <= 0) reset();
-  delay(100);
 }
